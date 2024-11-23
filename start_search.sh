2cbcaaa50db2ea7d070e7c66e6c8aa8ccf2da56e
Gustavo Couto <gustavokarlcouto@gmail.com>
2024-11-23 11:35:28 -0300

    Muda o nome da screen quando a flag -t eh acionada

--- start_search.sh
+++ start_search.sh
@@ -5,8 +5,10 @@ export PYTHONPATH=$PYTHONPATH:./api_carla/9.10/PythonAPI/carla/dist/carla-0.9.10
 
 if [ "$1" == "-t" ]; then
     script_name="search_hyperparameters_t.py"
+    screen_name="carla_bc_t"
 else
     script_name="search_hyperparameters.py"
+    screen_name="carla_bc"
 fi
 
-screen -L -S carla_expert .venv/bin/python $script_name
+screen -L -S "$screen_name" .venv/bin/python "$script_name"
